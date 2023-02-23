40c2494186652a4e7d545400f68834361af95598
Hamidechi Aimene <mhamidechi@sqli.com>
2023-02-23 13:38:08 +0100

    add definition

--- src/pages/CounterPage.tsx
+++ src/pages/CounterPage.tsx
@@ -10,6 +10,16 @@ export const Counter = () => {
       <CounterOne />
       <CounterTwo />
       <h3 className="second-title">counter avec custom hook </h3>
+      <h3>
+        In React, a custom hook is a reusable function that allows you to
+        encapsulate logic and state that can be shared across multiple
+        components. <br />
+        Custom hooks are just like regular hooks provided by React, but they are
+        created by the developers themselves to suit their specific use cases.
+        <br />
+        Custom hooks allow you to separate the concerns of your application into
+        smaller, reusable units.{" "}
+      </h3>
       <CounterWithHook />
     </div>
   );
